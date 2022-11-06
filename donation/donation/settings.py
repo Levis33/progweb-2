0d93cba2db0e0b09b99525def54168e7183fedd9
Levis33 <39637793+Levis33@users.noreply.github.com>
2022-11-06 19:31:54 -0300

    footer e navbar

--- donation/donation/settings.py
+++ donation/donation/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "project",
     "logger",
+    "fontawesomefree",
 ]
 
 MIDDLEWARE = [
@@ -122,7 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = 'static/'
+STATIC_DIR = 'static/'
+
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
